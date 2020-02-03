a354e75e491c210ef4f6c792a1b98704d6bdd44a
jayachandraredd <jayachandra016@gmail.com>
2020-02-03 19:28:25 +0530

    home

--- flightBooking/src/app/main/components/home/home.component.ts
+++ flightBooking/src/app/main/components/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpService } from 'src/app/shared/services/http.service';
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  selectDate: Date;
+  locations = [];
 
-  constructor() { }
+  constructor(private HttpService: HttpService) { }
 
+  
   ngOnInit() {
+
+    this.HttpService.get('/locations/getLocations').subscribe((data: any)=>{
+      console.log(data);
+      this.locations = data.locations;
+    })  
+    
   }
 
 }
